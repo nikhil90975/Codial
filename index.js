8ef572e5b6e6f2723e5fd047cf35dcd2b6235dba
nikhiltest9097 <nikhil.90975@gmail.com>
2020-03-22 21:23:56 +0530

    First Commit:create directory structure and setup express server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+//cghhvbkjnm
 app.listen(port,function(err){
     if(err){
         //interpolation-include variable inside string
