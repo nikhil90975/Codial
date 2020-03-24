3634ab15506e4a9c87616b1c91d8f6b73c07bc06
nikhil90975 <niks90975@gmail.com>
2020-03-24 12:53:36 +0530

    add view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for Codial</h1>');
+    res.render('home',{
+        title: "home"
+    });
 }
 
 console.log('controller loaded');
