b251308a6cbbf0e67ab390274ab85b905d9fd6ec
nikhil90975 <niks90975@gmail.com>
2020-05-10 18:06:40 +0530

    add gulp to compress

--- config/environment.js
+++ config/environment.js
@@ -67,4 +67,4 @@ const production = {
     }
 }
 
-module.exports = eval(process.env.CODIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODIAL_ENVIRONMENT);
+module.exports = development;
